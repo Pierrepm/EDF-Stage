aa859b0f84bef36c74a515e0a474a7e618e7423b
Pierrepm <pierre.pechotmaffre@gmail.com>
2012-10-29 10:06:50 +0100

    Mise en place dataTables

--- public/app.js
+++ public/app.js
@@ -33,6 +33,16 @@ App.Views.ClientsListView = Backbone.View.extend({
     render : function () {
         var renderedContent = Mustache.to_html(this.template, {clients : this.collection.toJSON()} );
         this.$el.html(renderedContent);
+		$(document).ready(function() {
+			$('#example').dataTable( {
+				"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+				"sPaginationType": "bootstrap",
+				"oLanguage": {
+					"sLengthMenu": "_MENU_ records per page"
+				}
+			} );
+		});
+		
     }
 });
 
--- public/main.js
+++ public/main.js
@@ -1,9 +1,11 @@
 ﻿yepnope({
     load: {
-        jquery              : 'assets/javascripts/jquery-1.8.2.js',
-        underscore          : 'assets/javascripts/underscore.js',
+        jquery              : 'assets/javascripts/jquery-1.8.2.js',	
+		underscore          : 'assets/javascripts/underscore.js',
         backbone            : 'assets/javascripts/backbone.js',
         mustache            : 'assets/javascripts/mustache.js',
+		dataTables          : 'assets/javascripts/jquery.dataTables.js',
+		DTBootstrap			: 'assets/javascripts/dataTables.bootstrap.js',
 		application         : 'assets/app.js'
     },
     complete : function () {
